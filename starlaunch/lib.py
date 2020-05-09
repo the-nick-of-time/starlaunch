0fdf68c848e77ea56a3fbfcb6d44d02ddbe37b78
Nick Thurmes <nthurmes@gmail.com>
2020-05-08 23:42:30 -0600

    Base functionality should be done now

--- starlaunch/lib.py
+++ starlaunch/lib.py
@@ -1,6 +1,8 @@
 import json
 import re
+import subprocess
 from pathlib import Path
+from tempfile import NamedTemporaryFile
 
 
 class NoBaseDir(Exception):
@@ -8,6 +10,10 @@ class NoBaseDir(Exception):
 
 
 class NoName(Exception):
+    pass
+
+
+class NoInstancesDir(Exception):
     pass
 
 
@@ -20,8 +26,18 @@ class ApplicationSettings:
     @property
     def starbound_dir(self) -> Path:
         if 'starbound_dir' in self.data:
-            return Path(self.data['starbound_dir'])
-        raise NoBaseDir('No base directory set')
+            directory = Path(self.data['starbound_dir'])
+            if directory.is_dir():
+                return directory
+        raise NoBaseDir('No or invalid base directory set')
+
+    @property
+    def instances_dir(self) -> Path:
+        if 'instances_dir' in self.data:
+            directory = Path(self.data['instances_dir'])
+            if directory.is_dir():
+                return directory
+        raise NoInstancesDir('No or invalid instances directory set')
 
     def write(self):
         with self.file.open('w') as f:
@@ -30,7 +46,7 @@ class ApplicationSettings:
 
 class Instance:
     def __init__(self, file: Path, settings: ApplicationSettings):
-        self.file = file
+        self.file = file.absolute()
         self.applicationSettings = settings
         with file.open('r') as f:
             self.data = json.load(f)
@@ -60,9 +76,26 @@ class Instance:
         with self.file.open('w') as f:
             json.dump(self.data, f, indent=2)
 
+    def config_file_contents(self) -> dict:
+        return {
+            "assetDirectories": [
+                str(self.applicationSettings.starbound_dir / '../assets'),
+                str(self.mods)
+            ],
+            "storageDirectory": str(self.storage)
+        }
+
+    def launch(self):
+        starbound = self.applicationSettings.starbound_dir / 'starbound'
+        # TODO: .autopatch file to change window name
+        # { "op": "replace", "path": "/windowTitle", "value": self.name }
+        with NamedTemporaryFile(mode='w+', encoding='utf8') as config:
+            json.dump(self.config_file_contents(), config)
+            subprocess.run([starbound, '-bootconfig', config.name])
+
 
 def make_path(path: str, sourcedir: Path, instancedir: Path) -> Path:
-    if re.match(r'/|[A-Za-z]:', path):
+    if re.match(r'^/|[A-Za-z]:', path):
         # absolute on either linux or windows
         return Path(path)
     if path.startswith('sb:'):
@@ -70,3 +103,5 @@ def make_path(path: str, sourcedir: Path, instancedir: Path) -> Path:
         return sourcedir / path[3:]
     if path.startswith('inst:'):
         return instancedir / path[5:]
+    raise ValueError('Invalid path given, must be absolute or relative to starbound (sb:) or '
+                     'instance (inst:) directory')
